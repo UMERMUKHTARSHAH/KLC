507363b07cf69951722ad8d1ab222612f3de60df
umer <shah.umer8493@gmail.com>
2026-08-24 12:55:24 +0530

    view

--- src/pages/Authentication/ViewUsers.jsx
+++ src/pages/Authentication/ViewUsers.jsx
@@ -163,6 +163,11 @@ const ViewUsers = () => {
       <div className="flex flex-col gap-6">
         {/* Main Card */}
         <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+         <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
+                <h3 className="font-medium text-slate-500 text-center text-xl dark:text-slate-400">
+                    View User
+                </h3>
+            </div>
           <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
             <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
               <div>
